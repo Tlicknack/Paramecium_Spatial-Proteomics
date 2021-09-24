d75d94792c454ce7447b18df2a89f797244cd4a6
Tlicknack <35544671+Tlicknack@users.noreply.github.com>
2021-09-24 10:47:59 -0700

    Update LOPIT-MarkerProteins.R
    Added info for BB and Cytoskeleton

--- LOPIT-MarkerProteins.R
+++ LOPIT-MarkerProteins.R
@@ -78,27 +78,81 @@ plotDist(ptetLOPIT12_NA_sumNormalized_markedY, markers = protY)                 
 cytoY = which(fData(ptetLOPIT12_NA_sumNormalized_markedY)["markersYeast"] == "Cytoplasm")
 plotDist(ptetLOPIT12_NA_sumNormalized_markedY, markers = cytoY)                    # only 1/3 peaks in Sup. another peaks in 30K?
 
-
-# Let's get into the GUI
+#####
+#####
+##### Let's get into the GUI
 pRolocGUI::pRolocVis(ptetLOPIT12_NA_sumNormalized_markedB, app = "pca", fcol = "markersBroad")
   # One by one, going through each group and adding paralogs in the dataset that weren't originally in the marker set
 
-# First complete: Basal Bodies
-ptetLOPIT12_NA_sumNormalized_markedBB = addMarkers(ptetLOPIT12_NA_sumNormalized, markers = "/data/LynchLabCME/Paramecium/Tim/Paramecium_LOPIT/markerProts/markersBB.csv", 
-                                                  mcol = "markersBB", fcol = "Accession", verbose = T)
-pRolocGUI::pRolocVis(ptetLOPIT12_NA_sumNormalized_markedBB, app = "pca", fcol = "markersBB")
-  #It appears as though there are 3 clusters for the BBs
+#### Basal Bodies
+  # After adding all paralogs and associated genes, I can upload my new list
+  #It appears as though there are 3 clusters for the BBs. I'll separate those below
     # 1st: Largest- Peaks in 300g
     # 2nd: Intermediate- Peaks in 3K
     # 3rd: Smallest- Peaks in Sup
-ptetLOPIT12_df_NA_sumNormalized_markedBB = exprs(ptetLOPIT12_NA_sumNormalized_markedBB[fData(ptetLOPIT12_NA_sumNormalized_markedBB)$"markersBB" == "BB"])
-nrow(ptetLOPIT12_df_NA_sumNormalized_markedBB)  # 45 proteins in the data
-ptetLOPIT12_df_NA_sumNormalized_markedBB_colMax = colnames(ptetLOPIT12_df_NA_sumNormalized_markedBB)[apply(ptetLOPIT12_df_NA_sumNormalized_markedBB,1,which.max)]
-table(ptetLOPIT12_df_NA_sumNormalized_markedBB_colMax)
-  #   MAC   Sup  X15K X300g   X3K 
-  #   24     4     1     8     8
-colNumbersBB = apply(ptetLOPIT12_df_NA_sumNormalized_markedBB,1,which.max)
-rowNumbersBB = c(1:length(colNumbersBB))
+  # Are these 3 clusters biologically meaningful??
+ptetLOPIT12_NA_sumNormalized_markedBB = addMarkers(ptetLOPIT12_NA_sumNormalized, markers = "/data/LynchLabCME/Paramecium/Tim/Paramecium_LOPIT/markerProts/markersBB.csv", 
+                                                  mcol = "markersBB", fcol = "Accession", verbose = T)  # Add our new markers. 64 total
+tmpN = which(fData(ptetLOPIT12_NA_sumNormalized_markedBB)$"markersBB" == "BB")    # Separate marker proteins for BB
+length(tmpN)                                                                      # 45 of 64 BB proteins in dataset
+tmpDF = exprs(ptetLOPIT12_NA_sumNormalized_markedBB[tmpN])                        # rows for BB markers
+tmpMax = colnames(tmpDF)[apply(tmpDF,1,which.max)]                                # column with largest value
+table(tmpMax)                                                   
+#   MAC   Sup  X15K X300g   X3K 
+#   24     4     1     8     8
+earlyBB = rownames(tmpDF[which(tmpMax == "MAC" | tmpMax == "X300g"),])            # 1st group: 300g and 1K
+midBB = rownames(tmpDF[which(tmpMax == "X3K"),])                                  # 2nd group: 3K
+lateBB = rownames(tmpDF[which(tmpMax == "X15K" | tmpMax == "Sup"),])              # Thirs group: Sup (and 15K, because where else would it go?)
+# PCA with each group highlighted to show good overlap between peaked columns and clusters
+plot2D(object = ptetLOPIT12_NA_sumNormalized_markedBB, fcol = NULL)             
+highlightOnPlot(object = ptetLOPIT12_NA_sumNormalized_markedBB, foi = earlyBB, col = "black", lwd = 2) 
+highlightOnPlot(object = ptetLOPIT12_NA_sumNormalized_markedBB, foi = midBB, col = "red", lwd = 2) 
+highlightOnPlot(object = ptetLOPIT12_NA_sumNormalized_markedBB, foi = lateBB, col = "green", lwd = 2) 
+
+#####
+##### Back to the GUI
+pRolocGUI::pRolocVis(ptetLOPIT12_NA_sumNormalized_markedB, app = "pca", fcol = "markersBroad")
+
+###### Actin
+  # Very few actine genes in data, so this is a place where using GO terms will be helpful
+  # Loading in all genes for the GOterm: 0015629 (Cytoskeleton)
+ptetLOPIT12_NA_sumNormalized_markedCytoskel = addMarkers(ptetLOPIT12_NA_sumNormalized, markers = "/data/LynchLabCME/Paramecium/Tim/Paramecium_LOPIT/markerProts/markersCytoskeleton.csv", 
+                                                   mcol = "markersCytosk", fcol = "Accession", verbose = T)  # Add our new markers. 64 total
+plot2D(object = ptetLOPIT12_NA_sumNormalized_markedCytoskel, fcol = "markersCytosk")             
+pRolocGUI::pRolocVis(ptetLOPIT12_NA_sumNormalized_markedCytoskel, app = "pca", fcol = "markersCytosk")
+  # Really interesting observation that PTET.51.1.P0710058 is the only protein spiking in the 3K fraction, while the rest peak in the Sup
+  # There's another protein peaking in 3K with a far lower relative intensity: PTET.51.1.P0600063
+    # THis is a paralog of a protein studied in Sehring et al. 2007 
+  # Another interesting protein is PTET.51.1.P0900031, which is higher in 9K, 12K, and 15K, but still peaks in the Sup. Maybe associated with heavier things? e.g. food vacuoles
+    # It's duplicate is PTET.51.1.P0770188, which only peaks in the SUp and doesnt increase elsewhere
+      # PTET.51.1.P0770188 clusters with the rest of the cytoskeleton
+  # Last one is PTET.51.1.P0320240, which peaks at 1K. Only protein of this group to do so
+    # It's duplicate is PTET.51.1.P0200282, which only peaks in Sup and clusters with most other Cytoskeleton
+tmpN = which(fData(ptetLOPIT12_NA_sumNormalized_markedCytoskel)$"markersCytosk" == "Cytoskeleton")    # Separate marker proteins for cytoskeleton
+length(tmpN)                                                                      # 31 of 61 proteins in dataset
+tmpDF = exprs(ptetLOPIT12_NA_sumNormalized_markedCytoskel[tmpN])                        # rows for cyto markers
+tmpMax = colnames(tmpDF)[apply(tmpDF,1,which.max)]                                # column with largest value
+table(tmpMax)                # let's break this into heavy and light clusters                                   
+#   MAC Sup X1K X3K X5K 
+#   3   24   1   2   1 
+heavyCytosk = rownames(tmpDF[which(tmpMax == "MAC" | tmpMax == "X300g" | tmpMax == "X1K" | tmpMax == "X3K" | tmpMax == "X5K"),])
+lightCytosk = rownames(tmpDF[which(tmpMax == "Sup"),])                      
+plot2D(object = ptetLOPIT12_NA_sumNormalized_markedCytoskel, fcol = NULL)             
+highlightOnPlot(object = ptetLOPIT12_NA_sumNormalized_markedCytoskel, foi = heavyCytosk, col = "black", lwd = 2) 
+highlightOnPlot(object = ptetLOPIT12_NA_sumNormalized_markedCytoskel, foi = lightCytosk, col = "red", lwd = 2) 
+
+#####
+##### Back to the GUI
+pRolocGUI::pRolocVis(ptetLOPIT12_NA_sumNormalized_markedB, app = "pca", fcol = "markersBroad")
+
+
+
+
+
+
+
+
+
 
 
 
