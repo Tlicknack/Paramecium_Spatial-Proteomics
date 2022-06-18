60480d5667816f18ec7ab5248cc6541c1279c6f9
Tim Licknack <35544671+Tlicknack@users.noreply.github.com>
2022-06-18 14:34:39 -0700

    just lists of colnames corresponding to WGD1/2/3 pairs

---
+++ WGDpairs.R
@@ -0,0 +1,35 @@
+  # WGDpairs.R
+
+wgd1_1 = c("ID1", "ID2")  # wgd1 pairs
+wgd1_2 = c("ID3", "ID4")
+wgd1_3 = c("ID5", "ID6")
+wgd1_4 = c("ID7", "ID8")
+
+wgd2_1 = c("ID1", "ID3")  # wgd2 pairs
+wgd2_2 = c("ID1", "ID4")
+wgd2_3 = c("ID2", "ID3")
+wgd2_4 = c("ID2", "ID4")
+wgd2_5 = c("ID5", "ID7")
+wgd2_6 = c("ID5", "ID8")
+wgd2_7 = c("ID6", "ID7")
+wgd2_8 = c("ID6", "ID8")
+
+wgd3_1 = c("ID1", "ID5")  # wgd3 pairs
+wgd3_2 = c("ID1", "ID6")
+wgd3_3 = c("ID1", "ID7")
+wgd3_4 = c("ID1", "ID8")
+
+wgd3_5 = c("ID2", "ID5")
+wgd3_6 = c("ID2", "ID6")
+wgd3_7 = c("ID2", "ID7")
+wgd3_8 = c("ID2", "ID8")
+
+wgd3_9 = c("ID3", "ID5")
+wgd3_10 = c("ID3", "ID6")
+wgd3_11 = c("ID3", "ID7")
+wgd3_12 = c("ID3", "ID8")
+
+wgd3_13 = c("ID4", "ID5")
+wgd3_14 = c("ID4", "ID6")
+wgd3_15 = c("ID4", "ID7")
+wgd3_16 = c("ID4", "ID8")
