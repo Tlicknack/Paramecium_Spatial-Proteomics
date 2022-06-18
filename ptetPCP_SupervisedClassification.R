7c8c396e4dcd8a450ebe18ab2555d9b6e3f9f1e7
Tim Licknack <35544671+Tlicknack@users.noreply.github.com>
2022-06-18 14:40:40 -0700

    Update ptetPCP_SupervisedClassification.R
    updates

--- ptetPCP_SupervisedClassification.R
+++ ptetPCP_SupervisedClassification.R
@@ -13,12 +13,14 @@ processingData(svmresMarked)
 preds1 = getPredictions(svmresMarked, fcol = "svm", mcol = "markers")   
 medSVM = median(fData(svmresMarked)$svm.scores)  #median svm score: .53
 preds2 = getPredictions(svmresMarked, fcol = "svm", t = medSVM, mcol = "markers") 
-
+ts1 = tapply(fData(preds1)$'svm.scores', fData(preds1)$'svm', median)
+preds3 = getPredictions(svmresMarked, fcol = "svm", t = ts1, mcol = "markers") 
+  
 #save.image("./ptetPCP_rdata/ptetPCP_SVM.Rdata")
 load("./ptetPCP_rdata/ptetPCP_SVM.Rdata")
 write.csv(fData(preds2), "./ptetPCP_csv/SVMpredictions.csv", row.names = F, quote = F)
   # Plugged the gene list into Biomart for properties. Manually assembled into PCP_properties.csv
-
+write.csv(data.frame(ts1), file = "/ptetPCP/ptetPCP_csv/ptetPCP_SVMscores_median.csv", quote = F, row.names = F)
 
 #
 # tSNE
@@ -28,6 +30,9 @@ addLegend(preds2, fcol = "svm", where = "topright", bty = "n", cex = .6)
 set.seed(42)
 plot2D(preds2, fcol = "svm.pred", method = "t-SNE")
 addLegend(preds2, fcol = "svm.pred", where = "topright", bty = "n", cex = .6)
+set.seed(42)
+plot2D(preds3, fcol = "svm.pred", method = "t-SNE")
+addLegend(preds3, fcol = "svm.pred", where = "topright", bty = "n", cex = .6)
 
 # Summary Table
 library(gt)
@@ -44,6 +49,7 @@ preds2_compare$Description = c("Core Basal Body", "Ciliary- Basal Body", "Cytoso
 preds2_compare = preds2_compare %>% 
   bind_rows(summarise_all(., ~if(is.numeric(.)) sum(.) else "Total"))
 preds2_compare$Description[18] = "."
+preds2_compare$SVMmed = as.numeric(ts1)
 write.csv(preds2_compare, "./ptetPCP_csv/SVMpredictions_Summary.csv", row.names = F, quote = F)
 
 preds2_compare %>% gt() %>% cols_align(align = "center") 
@@ -60,6 +66,8 @@ ggplot(knnsvm) + geom_count(aes(x=Cluster, y=svm.pred, color = ..n.., size = ..n
   xlab("De Novo Cluster") + ylab("SVM Predicted Compartment")
 
 
-
+# Klebsiella?
+table(fData(preds2[grep(pattern = "YP_*", x = fData(preds2)$'Accession')])$'svm.pred')
+plotDist(preds2[grep(pattern = "YP_*", x = fData(preds2)$'Accession')], fcol = "svm.pred")
 
 
